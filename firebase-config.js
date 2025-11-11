7850a5314715954c2e34102e1177752a94b7c033
Jehuti-ms <dmoseley@gams.edu.bb>
2025-11-11 19:01:54 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js - WorklogPro Configuration
 const worklogProConfig = {
-  apiKey: "AIzaSyBxOsRjT2m8Q1XqYcP67n8M9vZcW...",
-  authDomain: "worklogpro.firebaseapp.com",
-  projectId: "worklogpro",
-  storageBucket: "worklogpro.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abc123def456..."
+  apiKey: "AIzaSyDdLP_LgiC6EgzC3hUP_mGuNW4_BUEACs8",
+  authDomain: "worklogpro-4284e.firebaseapp.com",
+  projectId: "worklogpro-4284e",
+  storageBucket: "worklogpro-4284e.firebasestorage.app",
+  messagingSenderId: "299567233913",
+  appId: "1:299567233913:web:7232a5a5a8aa9b79948da8",
 };
 
 export default worklogProConfig;
