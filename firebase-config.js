6c6aaac74517ffbb7dcc6d0a5a780a50e88628a1
Jehuti-ms <dmoseley@gams.edu.bb>
2025-11-11 18:45:29 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js - WorklogPro Configuration
+const worklogProConfig = {
+  apiKey: "AIzaSyBxOsRjT2m8Q1XqYcP67n8M9vZcW...",
+  authDomain: "worklogpro.firebaseapp.com",
+  projectId: "worklogpro",
+  storageBucket: "worklogpro.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abc123def456..."
+};
+
+export default worklogProConfig;
